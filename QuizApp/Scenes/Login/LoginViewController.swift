82342e8b3b76aa61758f5cc8571ab3729aba6677
Giorgi Makadze <georgemakadze@gmail.com>
2023-06-03 11:25:46 +0400

    Remove story board

--- QuizApp/Scenes/Login/LoginViewController.swift
+++ QuizApp/Scenes/Login/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  QuizApp
 //
 //  Created by Giorgi Makadze on 02.06.2023.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
