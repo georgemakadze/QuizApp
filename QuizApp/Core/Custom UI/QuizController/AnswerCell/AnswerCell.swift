91649e7300c02a2dbc903d0fcf9ecc92aff74ffb
Giorgi Makadze <georgemakadze@gmail.com>
2023-06-07 18:47:02 +0400

    Add answercell constants

--- QuizApp/Core/Custom UI/QuizController/AnswerCell/AnswerCell.swift
+++ QuizApp/Core/Custom UI/QuizController/AnswerCell/AnswerCell.swift
@@ -12,8 +12,8 @@ class AnswerCell: UITableViewCell {
     
     private lazy var containerView: UIView = {
         let containerView = UIView()
-        containerView.backgroundColor = UIColor(hex: "F6F6F6")
-        containerView.layer.cornerRadius = 22
+        containerView.backgroundColor = Constans.ContainerView.backgroundColor
+        containerView.layer.cornerRadius = Constans.ContainerView.cornerRadius
         containerView.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(containerView)
         return containerView
@@ -22,7 +22,7 @@ class AnswerCell: UITableViewCell {
     private lazy var answerLabel: UILabel = {
         let answerLabel = UILabel()
         answerLabel.backgroundColor = .clear
-        answerLabel.font = .systemFont(ofSize: 16)
+        answerLabel.font = .systemFont(ofSize: Constans.AnswerLabel.font)
         answerLabel.textColor = .black
         answerLabel.translatesAutoresizingMaskIntoConstraints = false
         containerView.addSubview(answerLabel)
@@ -46,23 +46,43 @@ class AnswerCell: UITableViewCell {
     
     private func setupContainerViewConstraints() {
         NSLayoutConstraint.activate([
-            containerView.topAnchor.constraint(equalTo: contentView.topAnchor,constant: 2),
-            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,constant: 16),
-            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,constant: -16),
-            containerView.widthAnchor.constraint(equalToConstant: 340),
-            containerView.heightAnchor.constraint(equalToConstant: 60)
+            containerView.topAnchor.constraint(equalTo: contentView.topAnchor,constant: Constans.ContainerView.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,constant: Constans.ContainerView.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,constant: Constans.ContainerView.trailingAnchor),
+            containerView.widthAnchor.constraint(equalToConstant: Constans.ContainerView.widthAnchor),
+            containerView.heightAnchor.constraint(equalToConstant: Constans.ContainerView.heightAnchor)
         ])
     }
     
     private func setupAnswerLabelConstraints() {
         NSLayoutConstraint.activate([
-            answerLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 22),
-            answerLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -22),
-            answerLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 30)
+            answerLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: Constans.AnswerLabel.topAnchor),
+            answerLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: Constans.AnswerLabel.bottomAnchor),
+            answerLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: Constans.AnswerLabel.leadingAnchor)
         ])
     }
     
     func configure(with answer: String) {
         answerLabel.text = answer
+    }
+}
+
+extension AnswerCell {
+    enum Constans {
+        enum ContainerView {
+            static let backgroundColor = UIColor(hex: "F6F6F6")
+            static let cornerRadius: CGFloat = 22
+            static let topAnchor: CGFloat = 2
+            static let leadingAnchor: CGFloat = 16
+            static let trailingAnchor: CGFloat = -16
+            static let widthAnchor: CGFloat = 340
+            static let heightAnchor: CGFloat = 60
+        }
+        enum AnswerLabel {
+            static let font: CGFloat = 16
+            static let topAnchor: CGFloat = 22
+            static let bottomAnchor: CGFloat = -22
+            static let leadingAnchor: CGFloat = 30
+        }
     }
 }
